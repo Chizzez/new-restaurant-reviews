48b93301233ac70b75620488767f31b5ca88996f
Chizzez <s174272@student.dtu.dk>
2022-04-02 16:53:35 +0200

    last edits

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RestaurantList() {
+    return (
+      <div className="App">
+        Hello Hungry People!
+      </div>
+    );
+  }
+  
+  export default RestaurantList;
